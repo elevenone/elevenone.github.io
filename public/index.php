7115e70403e57e5901783b901e276b7bb5e2871d
mikka <ezsolt@gmail.com>
2020-11-12 18:25:03 +0100

    test

--- public/index.php
+++ public/index.php
@@ -3,12 +3,12 @@
  * stardust / a project skeleton for darkmatter
  */
 declare(strict_types = 1);
-echo 'ok?';
-exit;
+
 // date_default_timezone_set('UTC');
 
 // running under built-in server route static assets and return false
 if (php_sapi_name() == "cli-server") {
+    echo 'built-in server';
     $extensions = array("php", "jpg", "gif", "css", "png", "js", "ico", "svg");
     $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
     $ext = pathinfo($path, PATHINFO_EXTENSION);

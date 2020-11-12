6499ee20c160e5f9130561afc4498a475e6c9ab7
mikka <ezsolt@gmail.com>
2020-11-12 18:06:34 +0100

    public / ui

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * stardust / a project skeleton for darkmatter
+ */
+declare(strict_types = 1);
+
+// date_default_timezone_set('UTC');
+
+// running under built-in server route static assets and return false
+if (php_sapi_name() == "cli-server") {
+    $extensions = array("php", "jpg", "gif", "css", "png", "js", "ico", "svg");
+    $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+    $ext = pathinfo($path, PATHINFO_EXTENSION);
+    if (in_array($ext, $extensions)) {
+        return false;
+    }
+}
+
+// vendor
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+// commencing countdown
+require dirname(__DIR__) . '/app/boot.php';
